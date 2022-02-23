636c3325153de9141437a9f62f8851997f713890
OtsoLap <otsolappalainen@gmail.com>
2022-02-23 21:11:02 +0200

    done for tonight

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -40,8 +40,45 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
-// like a post
+// like/dislike a post
+router.put('/:id/like', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } })
+            res.status(200).json('this post has been liked')
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } })
+            res.status(200).json('this post has been disliked!')
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 // get a post
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // get timeline posts of followings
+router.get('/timeline/all', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId)
+        const userPosts = await Post.find({ userId: currentUser._id })
+        const friendPosts = await Promise.all(
+            currentUser.following.map(friendId => {
+                return Post.find({ userId: friendId })
+            })
+        )
+        res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
