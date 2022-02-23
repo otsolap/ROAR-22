c6e8766ff92d4dd20601ec6427f9553cd613e497
OtsoLap <otsolappalainen@gmail.com>
2022-02-23 13:51:17 +0200

    bcrypt added with postman testing. login and reg work atm.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,38 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 // Register user
-router.get('/register', async (req, res) => {
-    const user = await new User({
-        username: 'Otso',
-        email: 'otso@email.com',
-        password: 'secret1234',
-    })
+router.post('/register', async (req, res) => {
+    try {
+        // securing new password for users.
+        const salt = await bcrypt.genSalt(10)
+        const hashedPW = await bcrypt.hash(req.body.password, salt)
+        // create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPW,
+        })
 
-    await user.save()
-    res.send('ok')
+        // save user & return response
+        const user = await newUser.save()
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err)
+    }
 })
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        !user && res.status(404).send("User not found")
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
 
 module.exports = router
