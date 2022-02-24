0590f673a52afbeba1a810cece98b3cbda3f3349
OtsoLap <otsolappalainen@gmail.com>
2022-02-24 13:07:41 +0200

    Sidebar done.

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,18 @@
+import Feed from "../../components/feed/Feed"
 import Header from "../../components/header/Header"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Notifications from "../../components/notifications-bar/Notifications"
+import './home.css'
 
 export default function Home() {
     return (
-        <Header />
-
+        <>
+            <Header />
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Notifications />
+            </div>
+        </>
     )
 }
